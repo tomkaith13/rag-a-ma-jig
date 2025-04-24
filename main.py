cbfdc9386a2b6f5bd68bccb935dcaf821df07f53
Bibin Thomas <bthomas@league.com>
2025-04-24 19:16:08 -0400

    added more changes and eval

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
 from llama_index.llms.google_genai import GoogleGenAI
-from llama_index.core import SimpleDirectoryReader
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core import StorageContext
+from llama_index.vector_stores.docarray import DocArrayInMemoryVectorStore
+from llama_index.core.evaluation import FaithfulnessEvaluator
 
 
 
@@ -12,7 +15,7 @@ from google.cloud import aiplatform
 
 import os
 from dotenv import load_dotenv
-
+from llama_index.core import Settings
 
 
 
@@ -29,7 +32,7 @@ aiplatform.init(project=PROJECT_ID, location=LOCATION)
 llm = Vertex(model="gemini-2.0-flash-lite", temperature=0)
 
 embed_model = GoogleGenAIEmbedding(
-        model_name="text-embedding-005",
+        model_name="text-embedding-004",
         embed_batch_size=100,
         vertexai_config={
             "project": PROJECT_ID,
@@ -57,10 +60,36 @@ def main():
 
     docs = SimpleDirectoryReader("./data-files").load_data()
     print(f"Number of documents loaded: {len(docs)}")
-    for doc in docs:
-        print(f"Document: {doc}")
-        print(f"Text: {doc.text}")
-        print(f"Metadata: {doc.metadata}")
+    # for doc in docs:
+    #     print(f"Document: {doc}")
+    #     print(f"Text: {doc.text}")
+    #     print(f"Metadata: {doc.metadata}")
+    Settings.llm = llm
+    Settings.embed_model = embed_model  # Critical to prevent OpenAI fallback
+
+    vector_store = DocArrayInMemoryVectorStore()
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    
+    index = VectorStoreIndex.from_documents(
+        docs,
+        embedding=embed_model,
+        vector_store=vector_store,
+        storage_context=storage_context,
+        show_progress=True
+    )
+
+    query_engine = index.as_query_engine(
+        llm=llm,
+        similarity_top_k=3  # Retrieve top 3 relevant chunks
+    )
+    resp = query_engine.query("Is making fun of the candidate a good thing to do?")
+    print(f"Response: {resp}")
+    contexts = [node.node.get_content() for node in resp.source_nodes]
+
+    evaluator = FaithfulnessEvaluator(llm=llm)
+    eval_result = evaluator.evaluate(response=resp.response,contexts=contexts)
+    print(f"Evaluation Result: {eval_result.score}")
+
 
 if __name__ == "__main__":
     main()
