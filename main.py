dc1d73ad9b02701d9b9eee4d262027d907779db6
Bibin Thomas <bthomas@league.com>
2025-04-24 21:46:54 -0400

    added more changes

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from llama_index.llms.google_genai import GoogleGenAI
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 from llama_index.core import StorageContext
 from llama_index.vector_stores.docarray import DocArrayInMemoryVectorStore
-from llama_index.core.evaluation import FaithfulnessEvaluator
+from llama_index.core.evaluation import FaithfulnessEvaluator, RelevancyEvaluator, CorrectnessEvaluator
+from test_data.queries import queries
 
 
 
@@ -29,7 +30,8 @@ vertexai.init(project=PROJECT_ID,location=LOCATION)
 aiplatform.init(project=PROJECT_ID, location=LOCATION)  
 
 
-llm = Vertex(model="gemini-2.0-flash-lite", temperature=0)
+llm = Vertex(model="gemini-2.0-flash-lite", temperature=0.79)
+eval_llm = Vertex(model="gemini-2.0-flash-lite", temperature=0)
 
 embed_model = GoogleGenAIEmbedding(
         model_name="text-embedding-004",
@@ -47,16 +49,16 @@ def generate_embedding(text):
 # Example usage in the main function
 def main():
     print("Hello from llamaindex-rag!")
-    resp = llm.complete("What is the capital of France?")
-    print(resp)
+    # resp = llm.complete("What is the capital of France?")
+    # print(resp)
 
     # Generate embedding for a sample text
-    sample_text = "Paris is the capital of France."
+    # sample_text = "Paris is the capital of France."
     
     
-    embeddings = generate_embedding("Google Gemini Embeddings.")
-    print(embeddings[:5])
-    print(f"Dimension of embeddings: {len(embeddings)}")
+    # embeddings = generate_embedding("Google Gemini Embeddings.")
+    # print(embeddings[:5])
+    # print(f"Dimension of embeddings: {len(embeddings)}")
 
     docs = SimpleDirectoryReader("./data-files").load_data()
     print(f"Number of documents loaded: {len(docs)}")
@@ -82,13 +84,39 @@ def main():
         llm=llm,
         similarity_top_k=3  # Retrieve top 3 relevant chunks
     )
-    resp = query_engine.query("Is making fun of the candidate a good thing to do?")
-    print(f"Response: {resp}")
-    contexts = [node.node.get_content() for node in resp.source_nodes]
 
-    evaluator = FaithfulnessEvaluator(llm=llm)
-    eval_result = evaluator.evaluate(response=resp.response,contexts=contexts)
-    print(f"Evaluation Result: {eval_result.score}")
+
+    faithfulness_evaluator = FaithfulnessEvaluator(llm=eval_llm)
+    relevance_evaluator = RelevancyEvaluator(llm=eval_llm)
+    correctnes_evaluator = CorrectnessEvaluator(llm=eval_llm)
+
+    for query in queries:
+        q = query['question']
+        a = query['answer']
+
+        print(f'*' * 50 + '\n')
+        print(f"Query: {q}")
+        resp = query_engine.query(q)
+        print(f"Response: {resp}")
+        contexts = [node.node.get_content() for node in resp.source_nodes]
+        # print(f"Contexts: {contexts}")
+        eval_result = faithfulness_evaluator.evaluate(response=resp.response,contexts=contexts)
+        print(f"Faithfulness Evaluation Result Score: {eval_result.score}")
+
+        relevance_result = relevance_evaluator.evaluate(response=resp.response,query=q,contexts=contexts)
+        print(f"Relevance Evaluation Result: {relevance_result.passing}")
+        print(f"Relevance Evaluation Response: {relevance_result.response}")
+
+        correctnes_result = correctnes_evaluator.evaluate(response=resp.response,query=q,contexts=contexts,answer=a)
+        print(f"Correctness Evaluation Feedback: {correctnes_result.feedback}")
+    
+    # resp = query_engine.query("What applications do I need setup before my shift?")
+    # print(f"Response: {resp}")
+    # contexts = [node.node.get_content() for node in resp.source_nodes]
+
+    # evaluator = FaithfulnessEvaluator(llm=llm)
+    # eval_result = evaluator.evaluate(response=resp.response,contexts=contexts)
+    # print(f"Evaluation Result: {eval_result.score}")
 
 
 if __name__ == "__main__":
